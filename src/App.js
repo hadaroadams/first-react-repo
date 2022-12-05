4c1f4ff775d2da08d30ece84f84f20e20f10f0f2
hadaroadams <hadaroadams1234@gmail.com>
2022-12-05 17:03:42 +0000

    react-app cat

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Cards from "./component/cats/cats";
+function App() {
+  return (
+    <div>
+      <Cards/>
+      <Cards/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/component/cats/cats.jsx
@@ -0,0 +1,14 @@
+export default function cards(){
+    return(
+        <>
+        <img 
+        style={{width:"200px"}}
+        src="https://media.istockphoto.com/id/1361394182/photo/funny-british-shorthair-cat-portrait-looking-shocked-or-surprised.jpg?b=1&s=170667a&w=0&k=20&c=nOa1R7PGaqOaQscx10FpA5ZNenMeDfs-k6VgmmuY4cc=" alt="cat" />
+        <div>
+            <p>just trying somthing</p>
+            <a href="">about cats</a>
+        </div>
+        </>
+    )
+
+}
